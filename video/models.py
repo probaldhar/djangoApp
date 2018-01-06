05d07aac0c87132bc627af7c02058fd28577b5d8
Probal Chandra Dhar <probal@schedulegofer.com>
2018-01-06 02:26:49 -0600

    initial django app

---
+++ video/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Video(models.Model):
+    title = models.CharField("Title", max_length=250)
+    embed_code = models.TextField("Embed Code")
