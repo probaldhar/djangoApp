02f3d32f993820a7dad4cc932ce319af3e1dc398
Probal Chandra Dhar <probal@schedulegofer.com>
2018-01-07 03:37:22 -0600

    improvement of code, addition of view+models

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,20 @@ from django.urls import path
 
 from . import views
 
+# Namespacing URL names
+app_name = 'polls'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+
+    # route for poll home
+    path('', views.IndexView.as_view(), name='index'),
+
+    # route for question detail page
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
+    # route for results page
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
+    # route for vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
